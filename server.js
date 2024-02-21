1ee0dbb3343663b03c414eeb18e99c17509363d5
System Administrator <root@MacBook-Air-de-NAVMAR.local>
2024-02-21 09:42:48 -0600

    added models

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/blog', (req, res) => {
+    res.send('Hello blog');
+});
 
 app.listen(3000, () => {
     console.log('server running on port 3000');
 });
+
+mongoose.connect('mongodb+srv://guillermoarechigaav:<password>@blogdev.genoslg.mongodb.net/?retryWrites=true&w=majority')
